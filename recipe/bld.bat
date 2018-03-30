c3281249f69b20b9ad3a783abbc09babcb8d1a2b
Michael Sarahan <msarahan@gmail.com>
2018-03-30 18:53:46 -0500

    cleanup from messy merge with CF

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 setlocal EnableDelayedExpansion
 
 mkdir build
@@ -15,38 +14,21 @@ cmake -G "NMake Makefiles" ^
       -DBUILD_SHARED:BOOL=ON ^
       -DBUILD_TESTS:BOOL=ON ^
       -DBUILD_BENCHMARKS:BOOL=OFF ^
-      "%SRC_DIR%"
-if errorlevel 1 exit 1
-
-cmake --build .
-if errorlevel 1 exit 1
-
-ctest
-if errorlevel 1 exit 1
-
-cmake --build . --target install
-if errorlevel 1 exit 1
-=======
-mkdir build
-cd build
-
-:: use nmake because vs2008 gets tripped up by $(configuration)
-cmake -G "NMake Makefiles" ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -DCMAKE_BUILD_TYPE:STRING=Release ^
-      -DBUILD_STATIC:BOOL=OFF ^
       -DPREFER_EXTERNAL_SNAPPY:BOOL=ON ^
       -DPREFER_EXTERNAL_LZ4:BOOL=ON ^
       -DPREFER_EXTERNAL_ZLIB:BOOL=ON ^
-      ..\
+      "%SRC_DIR%"
+if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build . --config Release
 if %ERRORLEVEL% neq 0 exit 1
+
+ctest -C reelase
+if %ERRORLEVEL% neq 0 exit 1
+
 cmake --build . --target INSTALL --config Release
 if %ERRORLEVEL% neq 0 exit 1
-ctest -C Release
 
 del %LIBRARY_BIN%\msvc*.dll
 del %LIBRARY_BIN%\Microsoft.*
 move %LIBRARY_LIB%\blosc.dll %LIBRARY_BIN%\
->>>>>>> use external libs for zlib, snappy.  add run_exports
--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,6 @@
 mkdir build
 cd build
 
-<<<<<<< HEAD
 cmake -G "Unix Makefiles" \
       -DCMAKE_BUILD_TYPE="Release" \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
@@ -12,21 +11,12 @@ cmake -G "Unix Makefiles" \
       -DBUILD_SHARED=1 \
       -DBUILD_TESTS=1 \
       -DBUILD_BENCHMARKS=0 \
-      CMAKE_CXX=${CXX} \
-      CMAKE_CC=${CC} \
-      "${SRC_DIR}"
-=======
-export LDFLAGS=${LDFLAGS}" -Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
-
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DBUILD_STATIC:BOOL=OFF \
       -DPREFER_EXTERNAL_SNAPPY:BOOL=ON \
       -DPREFER_EXTERNAL_LZ4:BOOL=ON \
       -DPREFER_EXTERNAL_ZLIB:BOOL=ON \
       CMAKE_CXX=${CXX} \
       CMAKE_CC=${CC} \
-      ..
->>>>>>> use external libs for zlib, snappy.  add run_exports
+      "${SRC_DIR}"
 
 cmake --build .
 cmake --build . --target install
